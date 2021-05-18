ae0dc705f70d33d357ba3b286ff39bdb9b807112
Elliot Stenning <elliot@elliotcomputerguy.com>
2021-05-18 11:00:16 +0100

    Remove-AzResourceGroup

--- remove-ResourceGroup.ps1
+++ remove-ResourceGroup.ps1
@@ -1,9 +1,16 @@
 
+
+Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
+# Install-Module azureRM <-- Depreciated and removed 2024
+# Connect-AzureRmAccount <-- Depreciated and removed 2024
+Connect-AzAccount
+
 # remove resource group without error captures. We can build on this to add multiple resource groups or 
 # just resources from resource groups and also check for accidental deletion etc. 
 
 $resourceName = Read-Host 'Enter the name of your resource group to remove:'
-Remove-AzureRMresourcegroup -name $resourceName
+# Remove-AzureRmResourceGroup <-- Depreciated and removed 2024
+Remove-AzResourceGroup -name $resourceName
 Write-Host $resourceName + 'has attempted to be removed.'
 
 
